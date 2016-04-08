af1e69899cedc366ee7889fba7a6d6468ac9b41e
Thomas <dorian@tmsp.ga>
2016-04-08 18:15:44 +0200

    Added Colors Enumeration

---
+++ Enums/Colors.pas
@@ -0,0 +1,29 @@
+// Common Tools library - Common function for working with Borland Pascal
+// Copyright (c) 2016 Dorian Stoll
+// Licensed under the terms of the MIT license
+
+// Definition for colors
+unit colors;
+
+interface
+	type
+		TColor = (Black= 0,
+  			  	  Blue = 1,
+  			  	  Green = 2,
+  			  	  Cyan = 3,
+  			  	  Red = 4,
+  			  	  Magenta = 5,
+  			  	  Brown = 6,
+  			  	  LightGray = 7,
+  			  	  DarkGray = 8,
+  			  	  LightBlue = 9,
+  			  	  LightGreen = 10,
+  			  	  LightCyan = 11,
+  			  	  LightRed = 12,
+  			 	  LightMagenta = 13,
+  			  	  Yellow = 14,
+  			 	  White = 15);
+					 
+implementation
+
+end.
